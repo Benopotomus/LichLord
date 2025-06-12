77b5151fc550bc741655fd9082dbd03d8b07e62a
Benopotomus <benopotomus@gmail.com>
2025-06-11 19:27:56 -0700

    Update PropStateComponent.cs

--- Assets/Scripts/Props/Components/PropStateComponent.cs
+++ Assets/Scripts/Props/Components/PropStateComponent.cs
@@ -42,6 +42,7 @@ namespace LichLord.Props
 
         public void AuthorityUpdate(float renderDeltaTime)
         {
+            /*
             switch (_currentState)
             {
                 case EPropState.HitReact:
@@ -63,6 +64,7 @@ namespace LichLord.Props
                     }
                     break;
             }
+            */
         }
     }
 }
